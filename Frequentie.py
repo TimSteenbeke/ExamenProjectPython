ab4fadaf84a275ef699c925a748fe11035c09207
timst <tim.steenbeke@student.kdg.be>
2018-01-13 13:13:13 +0100

    rangwaarde bepaalt ?

--- Frequentie.py
+++ Frequentie.py
@@ -7,23 +7,24 @@ def percentageQuestion(corpus, question):
     questionPercentage = 0
     for key in questionDict:
         if key in corpusDict.keys():
-            # questionPercentage += math.log10((l*questionDict[key]) + ((1-l) *corpusDict[key]))
-            questionPercentage += ((l*questionDict[key]) + ((1-l) *corpusDict[key]))
+            questionPercentage += math.log10((l*questionDict[key]) + ((1-l) *corpusDict[key]))
+            # questionPercentage += ((l*questionDict[key]) + ((1-l) *corpusDict[key]))
         else:
-            # questionPercentage += math.log10(questionDict[key])
-            questionPercentage += (questionDict[key])
+            questionPercentage += math.log10(questionDict[key])
+            # questionPercentage += (questionDict[key])
 
     question.setPercentage(questionPercentage)
 
     for comment in question.comments:
         commentdict = comment.dictionary
         commentPercentage = 0
-        for key in commentdict.keys():
-            if key in corpusDict.keys():
-                # commentPercentage += math.log10((l*commentdict[key]) + ((1-l) *corpusDict[key]))
-                commentPercentage += ((l * commentdict[key]) + ((1 - l) * corpusDict[key]))
-            else:
-                # commentPercentage += math.log10(commentdict[key])
-                commentPercentage += (commentdict[key])
-        comment.setPercentage(commentPercentage)
+        for key in questionDict.keys():
+            if key in commentdict.keys():
+                if key in corpusDict.keys():
+                    commentPercentage += math.log10((l*commentdict[key]) + ((1-l) *corpusDict[key]))
+                    # commentPercentage += ((l * commentdict[key]) + ((1 - l) * corpusDict[key]))
+                else:
+                    commentPercentage += math.log10(commentdict[key])
+                    # commentPercentage += (commentdict[key])
+            comment.setPercentage(commentPercentage)
 
